90d66bf44fec7ca9cff340d70818efc796bd37c5
Eric Weaver <eweaver@jwplayer.com>
2022-05-09 17:31:57 -0400

    Add more custom functions

--- profile.sh
+++ profile.sh
@@ -1,3 +1,5 @@
+export EDITOR='subl -w'
+
 # Add Brew to path
 if [[ -d /opt/homebrew/bin ]]; then
     export PATH="/opt/homebrew/bin:$PATH"
@@ -6,5 +8,53 @@ if [[ -d /opt/homebrew/sbin ]]; then
     export PATH="/opt/homebrew/sbin:$PATH"
 fi
 
+# Cache brew prefix
+BREW_PREFIX=$(brew --prefix)
+
 # Tab completion
 [[ -f "${BREW_PREFIX}/opt/bash-completion/etc/bash_completion" ]] && source "${BREW_PREFIX}/opt/bash-completion/etc/bash_completion"
+
+function epoch-to-human ()
+{
+  EPOCH="$1";
+  if [ -n "$EPOCH" ]; then
+      gdate -u -d "@$EPOCH" +'%F %r %Z';
+  fi
+}
+
+function utc-now {
+   gdate -u +%FT%TZ
+}
+
+function sdiff-curl {
+  sdiff -s <(curl -sfS "$1") <(curl -sfS "$2")
+}
+
+function sdiff-curl-jq {
+  sdiff -s <(curl -sfS "$1" | jq .) <(curl -sfS "$2" | jq .)
+}
+
+function getlogs {
+  local resource="$1"
+  local type
+  if [[ ${resource} == *"."* ]]; then
+    type="deployment"
+  else
+    type="project"
+  fi
+  open "$(boxer get "${type}" "${resource}" --link-logs)" 2>/dev/null || echo "Unable to open logs"
+}
+
+alias docker-clean='docker rm $(docker stop $(docker ps -aq))'
+
+# jenv
+if type -p jenv > /dev/null 2>&1; then
+    export PATH="$HOME/.jenv/bin:$PATH"
+    eval "$(jenv init -)"
+fi
+
+# pyenv
+if type -p pyenv > /dev/null 2>&1; then
+    eval "$(pyenv init -)"
+    type -p pyenv-virtualenv-init > /dev/null 2>&1 && eval "$(pyenv virtualenv-init -)"
+fi
